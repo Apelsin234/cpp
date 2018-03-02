92de5c8c843150beef7ea742ff5967f953393834
Alexander <ratatuy.1995@mail.ru>
2018-03-03 02:35:51 +0300

    q2

--- cpp-course_3-sem/promise_future/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/promise_future/promise_future/lib/googletest-master/googletest
-# Build directory: D:/promise_future/promise_future/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
